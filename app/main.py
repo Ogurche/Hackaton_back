4fe433a83fc4621b216f2f2735d52aa4123375a8
Harry <eishtein0000@gmail.com>
2023-09-19 01:44:22 +0300

    добавил поля и прикол сделал при старте сервера на sh файл не оброщать внимание 😡

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html
-
+from art import tprint
 import model
 from routes import router
 
@@ -10,25 +10,29 @@ model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
+# Заголовок панели
+@app.on_event("startup")
+async def startup():
+    tprint("Singletons",space=10)
+
+
+
 @app.get('/')
 async def home():
     return "За 100 рублей сделаю свой fastapi без orm и крудами"
 
 
 app.include_router(router, tags=["CRUD"])
-# app.include_router(router_user, prefix="/auth", tags=["auth"])
-# app.include_router(router, prefix="/cards", tags=["cards"])
-# app.include_router(router, prefix="/sessions", tags=["sessions"])
-# app.include_router(router, prefix="/transports", tags=["transports"])
-# app.include_router(router, prefix="/snils", tags=["snils"])
 
 
+# Swagger UI и OpenAPI документация
 @app.get("/docs", include_in_schema=False)
 async def custom_swagger_ui_html():
     return get_swagger_ui_html(
         openapi_url="/openapi.json",
         title="Custom Swagger UI"
     )
+
 
 @app.get("/openapi.json", include_in_schema=False)
 async def get_openapi():
